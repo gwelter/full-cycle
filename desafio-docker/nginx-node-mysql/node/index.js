88a827ad2315a71d5ed763257fd5717785243ba5
Guilherme Welter <guilherme.a.welter@gmail.com>
2022-01-01 17:19:53 -0300

    Docker compose with nginx pointing to the node server

--- desafio-docker/nginx-node-mysql/node/index.js
+++ desafio-docker/nginx-node-mysql/node/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+// const mysql = require("mysql");
 const app = express();
 
 const port = 3000;
+
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+
+// const connection = mysql.createConnection(config);
 
 app.get("/", (req, res) => {
   res.send("Full Cycle");
