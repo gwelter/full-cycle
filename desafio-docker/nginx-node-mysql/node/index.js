2d7aed2ce25984856c27a87d32cdcd5fc5bff25d
Guilherme Welter <guilherme.a.welter@gmail.com>
2022-01-01 18:31:26 -0300

    Displaying names

--- desafio-docker/nginx-node-mysql/node/index.js
+++ desafio-docker/nginx-node-mysql/node/index.js
@@ -13,8 +13,29 @@ const config = {
 
 // const connection = mysql.createConnection(config);
 
+const htmlForm = `
+<form action="/" method="post">
+  <input type="text" placeholder="Nome" name="name" id="name" />
+  <button type="submit">Salvar</button>
+</form>
+`;
+
+const htmlNames = (names) => `
+<ul>
+  ${names.map((name) => `<li>${name}</li>`).join("\n")}
+</ul>
+`;
+
+const names = ["Guilherme", "Gustavo", "Mirian"];
+
 app.get("/", (req, res) => {
-  res.send("Full Cycle");
+  let html = "";
+
+  html += htmlForm;
+  html += htmlNames(names);
+
+  res.set("Content-Type", "text/html");
+  res.end(Buffer.from(html));
 });
 
 app.listen(port, () => {
